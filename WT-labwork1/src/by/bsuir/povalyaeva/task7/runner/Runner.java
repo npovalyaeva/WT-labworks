ae1cd9e372ac4469e5351f66bd1cbeb9f772bc5f
Nadya Povalyaeva <iriejamaica@outlook.com>
2018-11-19 13:39:57 +0300

    Add task #7

--- WT-labwork1/src/by/bsuir/povalyaeva/task7/runner/Runner.java
+++ WT-labwork1/src/by/bsuir/povalyaeva/task7/runner/Runner.java
@@ -1,12 +1,21 @@
 package by.bsuir.povalyaeva.task7.runner;
 
+import by.bsuir.povalyaeva.DataScanner;
 import by.bsuir.povalyaeva.task7.util.Util;
 
 public class Runner {
     public static void main(String[] args) {
-        double[] unsortedArray = new double[]{4, 7, 1.9, 4, 1.5, 7.23, 0.5, 1, 4, 9, 1.56};
+
+        System.out.print("Enter length of array: ");
+        int length = DataScanner.enterNaturalNumber();
+        double[] unsortedArray = new double[length];
+
+        System.out.print("Enter elements of array: ");
+        for (int index = 0; index < length; index++)
+            unsortedArray[index] = DataScanner.enterDoubleNumber();
+
         double[] sortedArray = Util.sortByShell(unsortedArray);
         for (double i: sortedArray)
-            System.out.print(i + " ");
+            System.out.print(DataScanner.transformDoubleToUserFormat(i, "#.###") + " ");
     }
 }
